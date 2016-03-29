07e416f14d8c7d4b40d01b108554cbea725d7276
melroyanthony <themelroyanthony@gmail.com>
2016-03-29 16:40:24 +0530

    [COMPLETE] Second Commit

--- app/src/main/java/com/example/aero/localife/LocationTracer.java
+++ app/src/main/java/com/example/aero/localife/LocationTracer.java
@@ -9,5 +9,5 @@ public class LocationTracer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_tracer);
-    }
+    } 
 }
